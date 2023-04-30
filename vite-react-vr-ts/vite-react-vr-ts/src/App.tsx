515ed8cab56760d4a20bef2ba04abe8ea5a75642
wilbervargas <wilber.vargas@udea.edu.co>
2023-04-30 13:27:10 -0500

    build con exito

--- vite-react-vr-ts/vite-react-vr-ts/src/App.tsx
+++ vite-react-vr-ts/vite-react-vr-ts/src/App.tsx
@@ -1,40 +1,6 @@
-/*
-import React from 'react';
-
-
-const VRScene: React.FC = () => {
-  return (
-    <>
-   
-<html>
-    
-  <head>
-
-    <title>React VR 360</title>
-    <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
-  </head>
-
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/index.tsx"></script>
-  </body>
-
-</html>
-
-    </> 
-  );
-};
-
-export default VRScene;
-*/
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-import { useState } from 'react'
 import './App.css'
 import LogoReact from './LogoReact';
-import VrPlane from './VrPlane';
+
 import VrScene from './VrScene';
 
 function App() {
