a3f4f58d7919276f5edb47355ffa257b55bc4b69
wilbervargas <wilber.vargas@udea.edu.co>
2023-04-29 21:09:10 -0500

    intentando mostrar  pagina web con html2canvas

--- vite-react-vr-ts/vite-react-vr-ts/src/VRScene.tsx
+++ vite-react-vr-ts/vite-react-vr-ts/src/VRScene.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import 'aframe';
-import { Entity, Scene } from 'aframe-react';
 
-const LogoReact: React.FC = () => {
+import 'aframe';
+import { Entity, Scene, Sky, Text } from 'aframe-react';
+
+import React, { useEffect, useRef } from 'react';
+import html2canvas from 'html2canvas';
+
+const VRScene: React.FC = () => {
+  const planeRef = useRef(null);
+
+  useEffect(() => {
+
+    html2canvas(document.querySelector('#page')).then((canvas) => {
+        const dataURL = canvas.toDataURL();
+        // Asignar la imagen al material del plano
+        planeRef.current.setAttribute('material', { src: dataURL });
+      });
+
+    
+  }, []);
+
   return (
     <>
       <div>
         <h1>"vr"</h1>
         <Scene>
-            <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 1, z: -3}} />
+          <Entity id="page"
+            geometry={{ primitive: 'plane', width: 6, height: 4 }}
+            position={{ x: 0, y: 2, z: -3 }}
+            material={{ planeRef }}
+          />
         </Scene>
       </div>
-    </>  
+    </>
   );
 };
-export default LogoReact;
 
-/*
-
-*/
+export default VRScene;
