48b663d5d0eda96c21b0084217b2a83d6060d781
wilbervargas <wilber.vargas@udea.edu.co>
2023-05-06 18:53:58 -0500

    transparecio opacidad y textura

--- vite-react-vr/src/vrScene.jsx
+++ vite-react-vr/src/vrScene.jsx
@@ -1,18 +1,68 @@
 import React from 'react';
 import 'aframe';
-import { Entity, Scene } from 'aframe-react';
+import Cloud from './clouds.jpg';
 
 
 function VrScene(){
     return(
     <a-scene>
-      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+
+      <a-assets>
+
+        <a-mixin
+         id="frame"
+         geometry="primitive: plane; height: 2; width: 2"
+         material="transparent:true"
+         src= {Cloud}
+        />
+
+      </a-assets>
+
+      <a-entity id="ui" position="0 2.1 -2.5">
+
+        <a-entity id = "logo">
+          <a-image 
+            scale="0.5 0.5 0" 
+            src= {Cloud}
+            />
+        </a-entity >
+
+        <a-entity id ="music-frame">
+        </a-entity>
+
+      </a-entity>
+
+      <a-box 
+        position="-1 0.5 -3" 
+        rotation="0 45 0" 
+        color="blue"
+        src= {Cloud}
+        opacity ="0.5"
+      ></a-box>
+
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"  ></a-sphere>
+      
       <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-      <a-sky color="black"></a-sky>
+      
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" transparent="true" opacity="1.0" mixin="frame"
+      ></a-plane>
+      
+      <a-sky 
+        src="https://cdn.glitch.global/e20d5feb-5412-4c47-9272-48e617a890ee/nebulosa.jpg?v=1650590978196"
+      />
+
+      <a-entity
+        id="sky"
+        primitive="a-sky"
+        radius="100"
+        rotation="90 180 45"
+      />
+
+
     </a-scene>
     );
+
+   
 
 }
 export default VrScene;
