129a3702bcf25b2f63ca6d084c70d54bb14dcc1c
wilbervargas <wilber.vargas@udea.edu.co>
2023-05-05 20:57:18 -0500

    vista cilindrica

--- vite-react-vr-ts/src/VrScene.tsx
+++ vite-react-vr-ts/src/VrScene.tsx
@@ -9,16 +9,6 @@ const VrScene: React.FC = () => {
   return (
     <Scene>
       <Entity
-        id="plano"
-        geometry={{ primitive: 'plane', width: 20, height: 20 }}
-        position={{ x: 0, y: 0, z: 0 }}
-        rotation="270 0 0"
-        material={{ color: 'blue' }}
-        side="double"
-        
-      />
-
-      <Entity
         id="camara"
         primitive="a-camera"
         wasd-controls={{ acceleration: 10 }}
@@ -38,9 +28,18 @@ const VrScene: React.FC = () => {
       />
 
       <Entity
+        id="plano"
+        geometry={{ primitive: 'plane', width: 20, height: 20 }}
+        position={{ x: 0, y: 0, z: 0 }}
+        rotation="270 0 0"
+        material={{ color: 'black' }}
+        side="double"
+      />
+
+      <Entity
         id="pantalla"
-        geometry={{ primitive: 'plane', width: 3, height: 2 }}
-        position={{ x: 0, y: 2, z: -5 }}
+        geometry={{ primitive: 'plane', width: 10, height: 8 }}
+        position={{ x: 0, y: 4, z: -6 }}
         material={{ src: `url(${myImage})` }}
         side="double"
         collision="objects: #camara;"
@@ -49,31 +48,19 @@ const VrScene: React.FC = () => {
       <Entity 
         id='arco'
         geometry="primitive: ring; radiusInner: 2.5; radiusOuter: 3; thetaLength: 180;"  
-        material="color: red;"
-        position={{ x: 0, y: 0, z: 6 }}
+        material="color: red; side: double"
+        position={{ x: 0, y: 0, z: -7 }}
         rotation="0 0 0"
       />
 
       <Entity
-        id='cilindro'
-        geometry={{ primitive: 'cylinder', radius: 3, height: 3, openEnded: true, "theta-length": 180 }}
-        material={{ src: `url(${myImage})`, side: 'double' }}
-        position={{ x: 0, y: 10, z: -5 }}
-        rotation={{ x: 0, y: 180, z: 180 }}
-      />
-
-      <Entity
         id='curve'
-        geometry={{
-          primitive: "curve-plane",  //no funciona
-          height: 2,
-          radius: 3,
-        }}
-        material={{ color: "grey" }}
-        position={{ x: 0, y: 3, z: -3 }}
-        rotation={{ x: 0, y: 0, z: 0 }}
+        geometry="primitive: cylinder; radius: 8; height: 8; openEnded: true; thetaLength: 300;"  
+        //geometry={{ primitive: 'cylinder', radius: 3, height: 3, openEnded: true, 'theta-length': 290 }}
+        material={{ src: `url(${myImage})`, side: 'double' }}
+        position={{ x: 0, y: 4, z: 0 }}
+        rotation={{ x: 0, y: 210, z: 0 }}
       />
-
 
     </Scene>
   );
